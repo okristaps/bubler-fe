e8d5406a9ea3f5b09be924a70ae2ca1a554b828b
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-12 18:39:47 +0200

    elapsed time

--- src/app/game/components/game-area/game-info/game-info.js
+++ src/app/game/components/game-area/game-info/game-info.js
@@ -1,19 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
 import "./game-stats.css";
 
 export default function GameStats({ score, elapsedTime, lives }) {
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = ((time % 60000) / 1000).toFixed(0);
-    return `${minutes}:${seconds.padStart(2, "0")}`;
-  };
+  const [displayTime, setDisplayTime] = useState(elapsedTime);
 
-  console.log("elapsedTime", elapsedTime);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayTime(elapsedTime);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [elapsedTime]);
 
   return (
     <>
       <h1 className="top-middle">BUBLER</h1>
       <div className="top-container">
-        <div className="stat-box time top-left">⏳ {formatTime(elapsedTime)}</div>
+        <div className="stat-box time top-left">⏳ {displayTime}</div>
         <div className="stat-box lives top-right">❤️ {lives}</div>
       </div>
       <div className="stat-box score bottom-center">🏆 {score}</div>
