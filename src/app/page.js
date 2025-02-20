8dea74e5c2eea00059a46c89389357e109acd03c
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-20 20:38:32 +0200

    npm

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,8 @@ import AboutBUBLER from "../components/about-bubble";
 import GameplayHighlights from "../components/gameplay-highlights";
 import FutureVision from "../components/future-vision";
 import LegalDisclaimer from "../components/legal-disclaimer";
-import Footer from "../components/footer";
 import SocialsSection from "../components/socials";
 import RoadmapAndTokenomics from "../components/roadmap";
-import WelcomeBUBLER from "@/components/welcome";
 import Landing from "@/components/landing";
 
 export default function Home() {
