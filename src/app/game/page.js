23b45b70e1af64fae5cf7f4a8435a73d5370c320
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-22 21:23:42 +0200

    mystery bubble

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -26,8 +26,6 @@ export default function Game() {
     isDark,
   } = useGameWebSocket();
 
-  console.log("bubbles", bubbles);
-
   useEffect(() => {
     const preventZoom = (event) => {
       if (event.ctrlKey || event.metaKey || event.deltaY !== undefined) {
