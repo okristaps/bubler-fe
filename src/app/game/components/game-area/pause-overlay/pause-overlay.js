0eece97ccae98989ca3c55dec58628d80b7d3c33
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-23 21:03:04 +0200

    pause btn

--- src/app/game/components/game-area/pause-overlay/pause-overlay.js
+++ src/app/game/components/game-area/pause-overlay/pause-overlay.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./pause-overlay.css";
-import Image from "next/image";
 
 export default function PauseOverlay({ isPaused, onTogglePause, onEndGame }) {
   const [isMuted, setIsMuted] = useState(false);
@@ -33,10 +32,11 @@ export default function PauseOverlay({ isPaused, onTogglePause, onEndGame }) {
   return (
     <>
       {!isPaused && (
-        <button onClick={onTogglePause} className="absolute bottom-5 transition-transform duration-200 hover:scale-105">
-          <Image src="/vectors/pause.svg" alt="Pause" width={70} height={30} objectFit="cover" />
+        <button className="pause-overlay-button" onClick={onTogglePause}>
+          <img src="/game-icons/pause.png" alt="Pause" />
         </button>
       )}
+
       {isPaused && (
         <div className="pause-overlay">
           <div className="pause-overlay-content">
