8fb2a06094558a337590cc4bbc7cc3a3c0e82ccb
Kristaps Ozolins <107648450+okristaps@users.noreply.github.com>
2025-02-23 09:12:06 +0200

    bg audio

--- src/app/game/components/finished-screen/finished-screen.js
+++ src/app/game/components/finished-screen/finished-screen.js
@@ -3,18 +3,26 @@ import { useRouter } from "next/navigation";
 
 import "./finished-screen.css";
 import BubbleBackground from "@/components/bubble-bg";
+import { useEffect, useRef } from "react";
 
 export default function FinishedScreen({ score, elapsedTime, resetGame }) {
   const router = useRouter();
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = ((time % 60000) / 1000).toFixed(0);
-    return `${minutes}:${seconds.padStart(2, "0")}`;
-  };
+  const audioRef = useRef(null);
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.7;
+      audioRef.current.currentTime = 0;
+
+      audioRef.current.play().catch((err) => {
+        console.log("Failed to play loss audio:", err);
+      });
+    }
+  }, []);
 
   return (
     <div className="finished-screen">
       <BubbleBackground />
+      <audio ref={audioRef} src="/sounds/game-over.mp3" preload="auto" />
       <h1 className="text-[59px] text-center  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-2xl animate-bounce">
         Game over!
       </h1>
